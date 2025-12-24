e602c3ee9842cf5c584abae39af65d35b2ac05d4
AdaWorldAPI <jan@exo.red>
2025-12-24 14:44:13 +0100

    cleanup pages/2_sse.py

--- pages/2_sse.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-import json
-import time
-
-st.title("📡 SSE")
-st.json({"event": "endpoint", "data": "/message"})
-st.json({"event": "connected", "data": {"ts": time.time()}})
